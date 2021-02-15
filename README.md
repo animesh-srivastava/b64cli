9aad8eb8932ef76d12b9e9ab18f811415ba0353a
Animesh Srivastava <f20170523@hyderabad.bits-pilani.ac.in>
2021-02-15 14:17:16 +0530

    updated README, changed a few functions

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
+# Base 64 Command Line Interface
+
 A very short command line interface for encoding and decoding base64 messages.
 
+# Usage
+
+## Script
+	- For encoding
+		python b64cli.py --encode "Some String"
+
+	- For decoding
+		python b64cli.py --decode "VGhpcyBpcyBzb21lIHN0cmluZw=="
+
+	- Without copying to clipboard
+		python b64cli.py --encode "Some string you don't want copied" --copy False | no | n | 0
+
+![](usage.gif)
+
+# Setup 
+
+- This code runs using Pyperclip for copying the output on paperclip.
+- Depending on the OS, it would use pyperclip's backend dependencies.
+
+## Installation
+
+- For linux systems, copy the dist/b64cli to /usr/bin/b64cli for system-wide installation.
+
+## Building from source
+
+- For building from source, you'd need a python environment with following third party packages
+	- Pyperclip
+	- Pyinstaller
+
+- Test the script once by running b64cli.py with appropriate arguments
+
+- Run the following command from terminal
+	
+		pyinstaller b64cli.py
+
+# To-do
+
+- Build for Windows systems
+
+- Write a setup script
--- b64cli.py
+++ b64cli.py
@@ -1,6 +1,22 @@
-import argparse
-import base64
-import pyperclip
+from argparse import ArgumentParser, ArgumentTypeError
+from base64 import b64encode, b64decode
+from pyperclip import copy
+
+
+def string_to_boolean(value):
+    """
+    This is a misc function to assist argparse.
+    This function takes in an argument and returns the equivalent
+    Boolen value for it.
+    """
+    if isinstance(value, bool):
+        return value
+    if value.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif value.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise ArgumentTypeError('Boolean value expected.')
 
 
 def copy_to_clipboard(data):
@@ -8,44 +24,50 @@ def copy_to_clipboard(data):
     Takes in a string and copies it to the clipboad.
     Uses default copy-paste mechanism at the backend
     """
-    pyperclip.copy(data)
+    copy(data)
     print("Copied to clipboard!")
 
 
 def encode(string, errors="backslashreplace",
-           encoding="UTF-8", copy=True, print_to_console=True):
+           encoding="UTF-8", copy=True):
     """
     Takes in a string and some optional arguments and
     converts it to base64 encoded string.
     """
-    string_bytes = string.encode(encoding=encoding, errors=errors)
-    base64_bytes = base64.b64encode(string_bytes)
+    try:
+        string_bytes = string.encode(encoding=encoding, errors=errors)
+    except UnicodeEncodeError:
+        print("Could not encode string. Check if the string is in correct format and there are no illegal characters.")
+        return False
+    base64_bytes = b64encode(string_bytes)
     message = base64_bytes.decode(encoding)
-    if print_to_console:
-        print("{0}".format(message))
+    print("{0}".format(message))
     if copy:
         copy_to_clipboard(message)
     return message
 
 
 def decode(string, errors="backslashreplace",
-           encoding="UTF-8", copy=True, print_to_console=True):
+           encoding="UTF-8", copy=True):
     """
     Takes in an encoded string and some optional arguments and
     converts it back to human-readable string
     """
     base64_bytes = string.encode(encoding=encoding, errors=errors)
-    string_bytes = base64.b64decode(base64_bytes)
-    message = string_bytes.decode(encoding)
-    if print_to_console:
-        print("{0}".format(message))
+    string_bytes = b64decode(base64_bytes)
+    try:
+        message = string_bytes.decode(encoding)
+    except UnicodeDecodeError:
+        print("Could not decode string. Check if the input is a valid base64 string")
+        return False
+    print("{0}".format(message))
     if copy:
         copy_to_clipboard(message)
     return message
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
+    parser = ArgumentParser(
         prog="Base 64 CLI",
         description="A command line interface to encode and decode strings",
         epilog="Contributed by @animesh-srivastava"
@@ -53,24 +75,22 @@ if __name__ == "__main__":
 
     action = parser.add_mutually_exclusive_group(required=True)
     action.add_argument("--encode",
+                        "-e",
                         help="Encode input string")
     action.add_argument("--decode",
+                        "-d",
                         help="Decode input string")
-    parser.add_argument("-p",
-                        action="store_true",
-                        help="Print to console",
-                        default=True
-                        )
     parser.add_argument("-c",
-                        action="store_true",
+                        "--copy",
+                        type=string_to_boolean,
+                        nargs="?",
+                        const=True,
                         help="Copy to clipboard",
                         default=True)
     args = parser.parse_args()
     if args.encode:
         string = encode(args.encode,
-                        copy=args.c,
-                        print_to_console=args.p)
+                        copy=args.copy)
     elif args.decode:
         string = decode(args.decode,
-                        copy=args.c,
-                        print_to_console=args.p)
+                        copy=args.copy)
